bbeb7caf022767c47c453eba64a4232fd7541d06
Dean Lark <48802382+dtlark@users.noreply.github.com>
2022-05-27 15:59:49 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char** argv) {
+
+	//Sample derivative information 
+	float S = 101.15      //stock price
+	float K = 98.01       //strike price
+	float vol = 0.0991    //volatility
+	float r = 0.01        //risk-free rate
+	float N = 10          //number of time steps
+	float M = 1000        //number of simulations
+
+	return 0;
+}
